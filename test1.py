83c60c340ef5fbfd2df5d96c8e73227e236ad867
ShayanNazemi <nazemi.shayan@gmail.com>
2019-08-01 11:57:04 +0430

    First Commit

---
+++ test1.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+s = 0
+for i in range(100):
+    s += i
+
+print("Git tutorial 1")
---
+++ test2.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+def func add(x, y):
+    return x + y
+
+print(add(4, 5))
+print("Git tutorial 2")
